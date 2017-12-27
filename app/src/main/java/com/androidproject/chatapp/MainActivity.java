6dab740d08d70a6f40239bee8df3fcd38bfcd897
James Sarkar <james9999994@gmail.com>
2017-12-27 17:26:46 -0500

    Added menu for the main page and a log out menu item

--- app/src/main/java/com/androidproject/chatapp/MainActivity.java
+++ app/src/main/java/com/androidproject/chatapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Android Chat App");
+        getSupportActionBar().setTitle("ChatApp");
     }
 
     @Override
@@ -34,11 +36,37 @@ public class MainActivity extends AppCompatActivity {
 
         // If user is not logged in
         if (currentUser == null) {
-            // Redirect user to the start page
-            Intent intent = new Intent(MainActivity.this, StartActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+            logOutUser();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.log_out_button_main) {
+            mAuth.signOut();
+
+            logOutUser();
+        }
+
+        return true;
+    }
+
+    private void logOutUser() {
+        // Redirect user to the start page
+        Intent intent = new Intent(MainActivity.this, StartActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
